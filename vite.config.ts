d24879c1b0bfae755d72fb54e0e3e0c72d593aca
Joshua Bruner <josh.bruner15@yahoo.com>
2025-03-21 14:30:05 -0400

    Fix base path for concretejunglepm.com

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/concrete_Jungle/', // Added this line!
+  base: '/', // Added this line!
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
